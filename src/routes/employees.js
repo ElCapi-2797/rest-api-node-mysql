4759c7b7be29d3d4d10a387075dbe9f718e8ad40
AbelardoTM <abel_27_1997@hotmail.com>
2022-08-23 22:17:50 -0500

    Se agrega rutas put y delete

--- src/routes/employees.js
+++ src/routes/employees.js
@@ -5,6 +5,7 @@ const mysqlConnection = require('../database');
 
 //Request all the employees
 router.get('/',  (req,res) => {
+    
     mysqlConnection.query('SELECT * FROM employees', (err, rows, fields) => {
         if(!err){
             res.json(rows);
@@ -16,28 +17,60 @@ router.get('/',  (req,res) => {
 
 //Requeste a employee by Id
 router.get('/:id', (req,res) => {
+    
     const { id } = req.params;
+    
     mysqlConnection.query('SELECT * FROM employees WHERE id = ?', [id], (err,rows, fields) => {
         if(!err){
             res.json(rows);
         }else{
             console.log(err);
         }
-    });
+    }); 
 });
 
 //Create a new employee
 router.post('/', (req,res) => {
-    const { id, name, body } = req.body;
+    const { id, name, salary } = req.body;
+    
     const query = `
-        SET @id = ?;
-        SET @name = ?;
-        SET @salary = ?;
-        CALL employeeAddOrEdit(@id,@name,@salary);
+        CALL employeeAddOrEdit(?,?,?);
     `;
+    
     mysqlConnection.query(query, [id, name, salary], (err, rows, fields) => {
         if(!err){
             res.json({Status: 'Employeed Saved'});
+        }else{
+            console.log(err);
+        }
+    });
+});
+
+//Edit a employee
+router.put('/:id', (req,res) => {
+    const { name, salary } = req.body;
+    const { id } = req.params;
+
+    const query = 'CALL employeeAddOrEdit(?, ?, ?);';
+
+    mysqlConnection.query(query, [id, name, salary], (err, rows, fields) => {
+        if(!err){
+            res.json({Status: 'Employeed Updated'});
+        }else{
+            console.log(err);
+        }
+    });
+
+
+});
+
+//Delete a employee
+router.delete('/:id', (req,res) => {
+    const { id } = req.params;
+    
+    mysqlConnection.query('DELETE FROM employees WHERE id = ?', [id], (err, rows, fields) => {
+        if(!err){
+            res.json({Status: 'Employeed Deleted'});
         }else{
             console.log(err);
         }
